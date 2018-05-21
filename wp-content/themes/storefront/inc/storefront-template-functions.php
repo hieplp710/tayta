baf4cf9b48689c2e731f8cb43dd64b6f3b967e53
hiepquach <hieplp710@gmail.com>
2018-05-21 23:12:10 +0700

    init at master

--- wp-content/themes/storefront/inc/storefront-template-functions.php
+++ wp-content/themes/storefront/inc/storefront-template-functions.php
@@ -125,7 +125,7 @@ if ( ! function_exists( 'storefront_credit' ) ) {
 	function storefront_credit() {
 		?>
 		<div class="site-info">
-			<?php echo esc_html( apply_filters( 'storefront_copyright_text', $content = '&copy; ' . get_bloginfo( 'name' ) . ' ' . date( 'Y' ) ) ); ?>
+			<?php echo esc_html( apply_filters( 'storefront_copyright_text', $content = '&copy; ' . get_bloginfo( 'name' ) . ' 2014-' . date( 'Y' ) ) ); ?>
 			<?php if ( apply_filters( 'storefront_credit_link', true ) ) { ?>
 			<br />
 			<?php
@@ -133,7 +133,6 @@ if ( ! function_exists( 'storefront_credit' ) ) {
 					the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
 				}
 			?>
-			<?php echo '<a href="https://woocommerce.com" target="_blank" title="' . esc_attr__( 'WooCommerce - The Best eCommerce Platform for WordPress', 'storefront' ) . '" rel="author">' . esc_html__( 'Built with Storefront &amp; WooCommerce', 'storefront' ) . '</a>.' ?>
 			<?php } ?>
 		</div><!-- .site-info -->
 		<?php
